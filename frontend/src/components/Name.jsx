64eee3fab81fec657c78cbb4398b8364cce6949f
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-06-21 16:44:18 +0530

    Added new component Name.jsx

---
+++ frontend/src/components/Name.jsx
@@ -0,0 +1,8 @@
+
+export const Name = ({name}) => {
+    return <div className="flex">
+        <div className="text-lg">
+            Welcome <span className="font-bold text-blue-400">{name}</span>
+        </div>
+    </div>
+}
